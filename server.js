3df55dcc6bc4f3b8c306debe202849d281e76990
ParkYongHo1 <qkaxhf8823@naver.com>
2025-11-02 14:55:17 +0900

    Feat | 이벤트 위임을 통한 전역 이벤트 핸들러 구현적용

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 5500;
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
